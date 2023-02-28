d6eb218ebed6ce1d0a8164481ec7be8c63a2a91b
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-02-28 19:14:28 +0500

    ♻️ update : call the new component in the main app file

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { StyleSheet, View } from 'react-native';
 
 import  Header  from './components/Header';
+import GameScree from './components/StartGameScreen';
 
 export default function App() {
   return (
-    <View style={styles.screen}>
+    <View style={styles.screen}> 
      <Header title="Guess A Number" />
+     <GameScree></GameScree>
     </View>
   );
 }
