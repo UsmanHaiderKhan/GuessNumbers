59cc606580f32e978d3f501f17e528a005d8376e
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-03-01 17:15:51 +0500

    🎉 Add : New file for card component

---
+++ components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, StyleSheet, View, TextInput, Button } from "react-native";
+
+const Card = prop =>{
+    return (
+        <View>
+            <Text>Card Text</Text>
+        </View>
+    );
+}
+ const styles = StyleSheet.create({
+     mainCard: {
+        flex: 1,
+     }
+ });
+export default Card
