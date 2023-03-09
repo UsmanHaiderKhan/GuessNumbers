1ab1e45fad1d6ca6ab3e8b3e7c83f1bd3836d2ac
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-03-09 11:52:02 +0500

    ♻️ Update : add confirm button handler and show output after confirming

--- components/StartGameScreen.js
+++ components/StartGameScreen.js
@@ -1,37 +1,58 @@
 import React, { useState } from 'react';
+
+import {Text,TouchableWithoutFeedback,StyleSheet, View, Button, Keyboard } from "react-native";
+import Card  from './Card';
 import Color from '../constants/color';
 import Input from './Input';
-import {Text,TouchableWithoutFeedback,StyleSheet, View, Button, TouchableWithoutFeedback, Keyboard } from "react-native";
 
 const StartGameScreen = prop =>{
    const [enteredValue, setEnteredValue] = useState('');
+   const [confirm, setConfirm] = useState(false);
+   const [selectedNumber, setSelectedNumber]= useState('');
+
    const numberInputHandler = inputText => {
       setEnteredValue(inputText.replace(/[^0-9]/g, ''));
    }
    const resetInputHandler  = ()=>{
       setEnteredValue('');
+      setConfirm(false);
    }
+   const confirmInputHandler = ()=>{
+      const chosenNumber = parseInt(enteredValue);
+      if(chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99){
+         return;
+      }
+         setConfirm(true);
+         setSelectedNumber(chosenNumber);
+         setEnteredValue('');
+   }
+   let confirmedOutPut;
+   if(confirm){
+      confirmedOutPut = <Text>ChosenNumber : {selectedNumber}</Text>
+   }
+
     return (
       <TouchableWithoutFeedback onPress={()=>{Keyboard.dismiss();}}>
-        <View style={styles.screen}>
-            <Text style={styles.title}>The Game Screen is here</Text>
-            <View style={styles.inputContainer}>
-                <Text style={styles.title}>Select a Number</Text>
-                <Input style={styles.input} 
-                blurOnSubmit 
-                autoCapitalize='none' 
-                autoCorrect={false}
-                keyboardType='number-pad'
-                maxLength={2}
-                onChangeText={numberInputHandler}
-                value={enteredValue} />
-                <View style={styles.buttonContainer}>
-                  <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Color.secondary}/></View>  
-                  <View style={styles.button}><Button title="Confirm" onPress={()=> {}} color={Color.primary}/></View> 
-                </View>
-            </View>
-        </View>
-         </TouchableWithoutFeedback>
+         <View style={styles.screen}>
+               <Text style={styles.title}>The Game Screen is here</Text>
+               <Card style={styles.inputContainer}>
+                  <Text style={styles.title}>Select a Number</Text>
+                  <Input style={styles.input} 
+                  blurOnSubmit 
+                  autoCapitalize='none' 
+                  autoCorrect={false}
+                  keyboardType='number-pad'
+                  maxLength={2}
+                  onChangeText={numberInputHandler}
+                  value={enteredValue} />
+                  <View style={styles.buttonContainer}>
+                     <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color={Color.secondary}/></View>  
+                     <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color={Color.primary}/></View> 
+                  </View>
+               </Card>
+               {confirmedOutPut}
+         </View>
+      </TouchableWithoutFeedback>
      );
 };
 
@@ -46,17 +67,9 @@ const styles = StyleSheet.create({
         marginVertical: 10,
      },
      inputContainer: {
-        width:300,
-        maxWidth: '80%',
-        alignItems: 'center',
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: .26,
-        elevation: 8,
-        backgroundColor: 'white',
-        padding: 20,
-        borderRadius: 10,
+      width: 300,
+      maxWidth: '80%',
+      alignItems: 'center'
      },
      buttonContainer: {
         flexDirection: 'row',
