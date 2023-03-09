45cb155d4d05f6f1978f815c7ed0f35a5a4b66c8
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-03-09 11:51:14 +0500

    ♻️ Update : add card shadow styling

--- components/Card.js
+++ components/Card.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import {Text, StyleSheet, View, TextInput, Button } from "react-native";
+import { View, StyleSheet } from 'react-native';
 
-const Card = prop =>{
-    return (
-        <View>
-            <Text>Card Text</Text>
-        </View>
-    );
-}
- const styles = StyleSheet.create({
-     mainCard: {
-        flex: 1,
-     }
- });
-export default Card
+const Card = props => {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10
+  }
+});
+
+export default Card;
