34d4ade1ccfd3d46f9a9191a80f5835a4312a90d
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-03-21 18:07:34 +0500

    ♻️ Update : Call Game Over Screen Component

--- App.tsx
+++ App.tsx
@@ -4,15 +4,23 @@ import { StyleSheet, View } from 'react-native';
 import  Header  from './components/Header';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
-
+import GameOverScreen from './screens/GameOverScreen';
 export default function App() {
    const[userNumber, setUserNumber]  = useState();
+   const[guessRounds, setGuessRounds] = useState(0);
    const startGameHandler = (selectedNumber: React.SetStateAction<undefined>) => {
       setUserNumber(selectedNumber);
    };
+
+   const gameOverHandler = (numOfRounds: React.SetStateAction<number>) =>{
+    setGuessRounds (numOfRounds);
+   }
+
    let content = <StartGameScreen onStartGame ={startGameHandler}/>;
-   if(userNumber){
-    content =<GameScreen userChoice={userNumber}/>;
+   if(userNumber && guessRounds <= 0){
+    content =<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+   }else if(guessRounds > 0){
+    content = <GameOverScreen/>
    }
   return (
     <View style={styles.screen}> 
