2586f143b5c601134fd72a888d911ad4d0bfe4d5
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-03-27 16:03:51 +0500

    ♻️ Update : add no of rounds dynamically

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Button } from "react-native";
 
 const GameOverScreen = props =>{
     return(
     <View style={styles.screen}>
         <Text>Your Game is Over! </Text>
+        <Text>Number of rounds : {props.roundNumber}</Text>
+        <Text>Number was : {props.userNumber}</Text>
+
+        <Button title="NEW GAME" onPress={props.onRestart}/>
     </View>
     );
 };
@@ -16,9 +20,7 @@ const styles= StyleSheet.create({
         flex:1,
         padding:10,
         alignItems: "center",
-        justifyContent:'center',
-        
-
+        justifyContent: "center",
     }
 
 });
