59e1b2df6a05855dbeb3e53e7b3fb1321931a524
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-03-27 16:03:15 +0500

    🎉 Add : new game handler and numbers of round

--- App.tsx
+++ App.tsx
@@ -5,23 +5,32 @@ import  Header  from './components/Header';
 import GameScreen from './screens/GameScreen';
 import StartGameScreen from './screens/StartGameScreen';
 import GameOverScreen from './screens/GameOverScreen';
+
 export default function App() {
    const[userNumber, setUserNumber]  = useState();
    const[guessRounds, setGuessRounds] = useState(0);
-   const startGameHandler = (selectedNumber: React.SetStateAction<undefined>) => {
+
+   const newGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber();
+   };
+
+   const startGameHandler = (selectedNumber:any) => {
       setUserNumber(selectedNumber);
    };
 
-   const gameOverHandler = (numOfRounds: React.SetStateAction<number>) =>{
+   const gameOverHandler = (numOfRounds:any) =>{
     setGuessRounds (numOfRounds);
    }
 
-   let content = <StartGameScreen onStartGame ={startGameHandler}/>;
+   let content = <StartGameScreen onStartGame={startGameHandler}/>;
+
    if(userNumber && guessRounds <= 0){
     content =<GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
    }else if(guessRounds > 0){
-    content = <GameOverScreen/>
+    content = <GameOverScreen roundNumber={guessRounds} userNumber={userNumber} onRestart= {newGameHandler}/>
    }
+
   return (
     <View style={styles.screen}> 
      <Header title="Guess A Number" />
